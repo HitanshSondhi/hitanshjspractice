a693d2b6e9446dbb4c9953fcdb9e4e2af0ab8bdc
HitanshSondhi <hitansh.sondhi.30@gmail.com>
2024-06-01 17:40:22 +0530

    part 1

--- j1.js
+++ j1.js
@@ -1,2 +1,9 @@
-console.log("hitansh");
-console.log("hello");
+let emailid="hitanshsondhi"
+const account="hitansh123"
+var password="hitansh1234"
+city="delhi";
+
+/*
+use only let and const because var has scope problem */
+
+console.table([emailid,account,password,city]);
